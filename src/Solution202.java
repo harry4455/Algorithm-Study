1ed1ced78ffc8b06b0acfbe8209ac35424696c22
harry4455 <xoxolittlemonster4455@gmail.com>
2020-08-09 15:02:25 +0900

    200809 commit

---
+++ src/Solution202.java
@@ -0,0 +1,15 @@
+public class Solution202 {
+    public static void main(String[] args) {
+        int[][] jobs = {{0,3},{1,9},{2,6}};
+        solution(jobs);
+    }
+    public static int solution(int[][] jobs) {
+        int answer = 0;
+
+
+
+
+
+        return answer;
+    }
+}
---
+++ src/Solution203.java
@@ -0,0 +1,5 @@
+public class Solution203 {
+    public static void main(String[] args) {
+
+    }
+}
