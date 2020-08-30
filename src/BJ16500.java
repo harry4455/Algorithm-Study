6913ac0c63d9de63b9929cc6913a9fa789461e6b
harry4455 <xoxolittlemonster4455@gmail.com>
2020-08-30 11:08:14 +0900

    20200830 commit
    28일 29일 풀어본거까지 일단 올림

---
+++ src/BJ16500.java
@@ -0,0 +1,41 @@
+/*
+    #16500 문자열 판별
+
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class BJ16500 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String word = br.readLine();
+        int N = Integer.parseInt(br.readLine());
+        String[] words = new String[100];
+        int[] dp = new int[101];
+
+        for(int i=0; i<N; i++) {
+            words[i] = br.readLine();
+        }
+
+        dp[words.length] = 1;
+
+        for(int pos = words.length - 1; pos >= 0; pos--) {
+            for (int j = 0; j <= N; j++) {
+                if ((word.charAt(pos) == words[j].charAt(0)) && (dp[pos + words[j].length()] == 1)) {
+                    dp[pos] = 1;
+                    break;
+                } else {
+                    dp[pos] = 0;
+                }
+            }
+        }
+
+        System.out.println(dp[0]);
+
+    }
+}
--- src/Greedy05.java
+++ src/Greedy05.java
@@ -5,11 +5,54 @@
  union-find algorithm
  하나의 cycle이 만들어지지 않도록 Disjoint Set을 표현하는데 활용되는 알고리즘
 
+==========================================================
+
+ List 만들어서 비교
  */
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class Greedy05 {
+
+    public static void main(String[] args) {
+        int n = 4;  // 섬의 갯수
+        int[][] costs = {{0,1,1},{0,2,2},{1,2,5},{1,3,1},{2,3,8}};
+        System.out.println(solution(n, costs));
+    }
+
+    public static int solution(int n, int[][] costs) {
+        int answer = 0;
+        int[][] adj = new int[n][n];    // 인접한 섬들 배열 확인하기 [출발지, 도착지]
+        for(int i=0; i < costs.length; i++){
+            adj[costs[i][0]][costs[i][1]] = adj[costs[i][1]][costs[i][0]] = costs[i][2];    //  연결되어 있는 섬들마다 비용 정해놓기
+        }
+        boolean[] visit = new boolean[n];
+        List<Integer> connect = new ArrayList<>(n+1);
+        connect.add(0);
+        visit[0] = true;
+        while(connect.size() < n) {     // 리스트에 저장된 크기가 총 섬의 크기보다 작을 때
+            int min = Integer.MAX_VALUE;
+            int minIdx = -1;    // 최소 거리의 도착지 섬 인덱스
+            for(int island = 0; island < connect.size(); island++) {
+                int i = connect.get(island);    // 섬들 중에 count 해서 지우고 마지막으로 지운 섬
+                for(int j = 0; j < n; j++) {
+                    // 방문 안했고, i에서 j까지 연결은 되어있고, 그 값이 최소인가
+                    if(!visit[j] && adj[i][j] > 0 && adj[i][j] < min) {
+                        min = adj[i][j];
+                        minIdx = j;
+                    }
+                }
+            }
+            visit[minIdx] = true;
+            connect.add(minIdx);    // 리스트에 도착지 섬 추가
+            answer += min;
+        }
+        return answer;
+    }
+
+    /*  실패한 케이스
 
     static class Edge implements Comparable<Edge> {
         int from, to, cost;
@@ -22,6 +65,8 @@ public class Greedy05 {
 
         @Override
         public int compareTo(Edge o) {
+            System.out.println("차이값");
+            System.out.println(this.cost - o.cost);
             return this.cost - o.cost;
         }
     }
@@ -74,5 +119,5 @@ public class Greedy05 {
     private static int find(int n) {
         if(parent[n] == n) return n;
         return parent[n] = find(parent[n]);
-    }
+    }*/
 }
