c67c73173dd34ecadb7dfc900a9eb1f2f0c2a906
harry4455 <xoxolittlemonster4455@gmail.com>
2020-12-09 23:36:47 +0900

    20201209 LCS 연습풀이 commit

---
+++ src/BJ5582.java
@@ -0,0 +1,33 @@
+/*
+    #5582 공통 부분 문자열
+
+    LCS(Longest Common Subsequence)
+    전형적인 DP 문제 중 하나
+
+    str1 = ABRACADABRA
+    str2 = ECADADABRBCRDARA
+
+ */
+
+import java.util.Scanner;
+
+public class BJ5582 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[] ch1 = sc.next().toCharArray();
+        char[] ch2 = sc.next().toCharArray();
+        int[][] LCS = new int[ch1.length+1][ch2.length+1];
+
+        int ans = Integer.MIN_VALUE;
+        for(int i = 1; i <= ch1.length; i++) {
+            for(int j=1; j <= ch2.length; j++) {
+                if(ch1[i-1] == ch2[j-1]) {
+                    LCS[i][j] = LCS[i-1][j-1] + 1;
+                }
+                ans = Math.max(ans, LCS[i][j]);
+            }
+        }
+        System.out.println(ans);
+    }
+}
