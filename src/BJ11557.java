e6159bda4a52a22b74e5339b30e4c1eb5fe3240f
harry4455 <xoxolittlemonster4455@gmail.com>
2021-01-07 21:39:37 +0900

    20210107 commit

---
+++ src/BJ11557.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class BJ11557 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while(T-- > 0) {
+            int N = sc.nextInt();
+            int max = 0;
+            String ans = "";
+            for(int i=0; i<N; i++) {
+                String line = sc.next();
+                int soju = sc.nextInt();
+                if(max < soju) {
+                    max = soju;
+                    ans = line;
+                }
+            }
+            System.out.println(ans);
+        }
+    }
+}
---
+++ src/BJ2089.java
@@ -0,0 +1,27 @@
+/*
+ *    나누기 연산은 -2로
+ *    나머지 연산시에는 몫을 버리는게 아니라 올림 처리
+ */
+
+import java.util.*;
+
+public class BJ2089 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        List<Integer> list = new ArrayList<>();
+
+        if(N==0) {
+            System.out.println(N);
+        }
+
+        while(N!=0) {
+            list.add(Math.abs(N%-2));
+            N = (int)Math.ceil((double)N/-2);
+        }
+
+        for(int i=list.size()-1; i>=0; i--) {
+            System.out.print(list.get(i));
+        }
+    }
+}
