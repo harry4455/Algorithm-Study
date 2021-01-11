b5c63a6d9b79eb601927311a55e9b4b8b562ad3c
harry4455 <xoxolittlemonster4455@gmail.com>
2021-01-11 23:32:25 +0900

    20210111 commit

---
+++ src/BJ1026.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class BJ1026 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr1 = new int[N];
+        int[] arr2 = new int[N];
+
+        stk = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            arr1[i] = Integer.parseInt(stk.nextToken());
+        }
+
+        stk = new StringTokenizer(br.readLine());
+        for(int j=0; j<N; j++) {
+            arr2[j] = Integer.parseInt(stk.nextToken());
+        }
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        int s = 0;
+
+        for(int k=0; k<N; k++) {
+            s += (arr1[k] * arr2[N-k-1]);
+        }
+
+        System.out.println(s);
+
+    }
+}
