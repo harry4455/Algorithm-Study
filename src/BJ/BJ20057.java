402a6752ba08d6a48856be66a19148b118e3d18a
harry4455 <xoxolittlemonster4455@gmail.com>
2021-03-22 23:37:17 +0900

    210322 commit

--- src/BJ/BJ20057.java
+++ src/BJ/BJ20057.java
@@ -1,3 +1,13 @@
+/*
+
+    #20057 마법사 상어와 토네이도
+
+    삼성 역량테스트 기출문제
+
+
+
+ */
+
 package BJ;
 
 import java.io.BufferedReader;
@@ -7,11 +17,22 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BJ20057 {
+    static int N;
+    static int[][] A;
+    static int[] dx = {0,1,0,-1};   //토네이토의 x 이동 방향
+    static int[] dy = {-1,0,1,0};   //토네이토의 y 이동 방향
+    static int[] dc = {1,1,2,2};   // 토네이도의 각 방향으로 이동하는 횟수
+    static int[][] dsx = {{-1,1,-2,-1,1,2,-1,1,0}, {-1,-1,0,0,0,0,1,1,2},    //모래가 퍼지는 x방향
+            {1,-1,2,1,-1,-2,1,-1,0}, {1,1,0,0,0,0,-1,-1,-2}};
+    static int[][] dsy = {{1,1,0,0,0,0,-1,-1,-2},{-1,1,-2,-1,1,2,-1,1,0},    //모래가 퍼지는 y방향
+            {-1,-1,0,0,0,0,1,1,2},{1,-1,2,1,-1,-2,1,-1,0}};
+    static int[] sandRatio ={1,1,2,7,7,2,10,10,5};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        int[][] A = new int[N][N];
+        N = Integer.parseInt(br.readLine());
+        A = new int[N][N];
 
         for(int i=0; i<N; i++) {
             StringTokenizer stk = new StringTokenizer(br.readLine());
@@ -20,6 +41,15 @@ public class BJ20057 {
             }
         }
 
-        System.out.println(Arrays.deepToString(A));
+        int result = calculate(N/2, N/2);
+        System.out.println(result);
+
+
+        //System.out.println(Arrays.deepToString(A));
+    }
+
+    private static int calculate(int x, int y) {
+
+        return 0;
     }
 }
